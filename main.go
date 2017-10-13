12bf1c9feb605c633239ba9a4fad916b8fb5683a
DanielDaverio <dani@creativehothouse.com>
2017-10-13 12:04:33 +0200

    Add mgo depedency and mlab connection test

--- main.go
+++ main.go
@@ -4,12 +4,51 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var players Players
 
 func main() {
+
 	setupMockedData()
+
+	uri:="mongodb://mlab-dani:dani1234@ds117935.mlab.com:17935/godata"
+
+	sess, err := mgo.Dial(uri)
+	if err != nil {
+		fmt.Printf("Can't connect to mongo, go error %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Connected to db")
+
+	defer sess.Close()
+
+	sess.SetSafe(&mgo.Safe{})
+
+	collection := sess.DB("godata").C("user")
+
+	/*
+	err = collection.Insert(&Player{"1", "Ma", "Long", "The Dragon", "-"},
+							&Player{"2", "Timo", "Boll", "The Nice Guy", "-"})
+	if err != nil {
+		log.Fatal("Problem inserting data: ", err)
+		return
+	}
+	*/
+
+	result := Player{}
+	err = collection.Find(bson.M{"firstname": "Ma"}).One(&result)
+	if err != nil {
+		log.Fatal("Error finding record: ", err)
+		return
+	}
+
+	fmt.Println("Last name:", result.Lastname)
+
 	router := NewRouter()
 	log.Fatal(http.ListenAndServe(":"+port(), router))
 }
